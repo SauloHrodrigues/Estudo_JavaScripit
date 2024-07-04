1e8feb4cf727723fe812b9ad3d388059064057c7
Pedro-Cecilio <cardev604@gmail.com>
2024-07-04 14:50:04 -0300

    :sparkles: feat: adicinando operador ternário

---
+++ operador-ternario/index.js
@@ -0,0 +1,10 @@
+/* 
+    - É uma forma simples de expressar uma condição if-else em uma única linha
+    - sintaxe: condicao ? <O que acontece se for verdadeira> : <O que acontece se for falsa>;
+*/
+
+const numero = 5
+
+const resultado = numero % 2 === 0 ? "Par" : "Ímpar"
+
+console.log(resultado) // Imprimirá: Ímpar
